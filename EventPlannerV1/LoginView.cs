d4aa3097036cb64afca2f41f65f7a4f361b71a4b
Buddhi Adhikari <LordDraagonLive@users.noreply.github.com>
2019-11-21 10:21:25 +0000

    Added login View

--- EventPlannerV1/LoginView.cs
+++ EventPlannerV1/LoginView.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EventPlannerV1
 {
-    public partial class Form1 : Form
+    public partial class LoginView : Form
     {
-        public Form1()
+        public LoginView()
         {
             InitializeComponent();
         }
--- EventPlannerV1/Program.cs
+++ EventPlannerV1/Program.cs
@@ -16,7 +16,7 @@ namespace EventPlannerV1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginView());
         }
     }
 }
