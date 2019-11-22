264dc9372088500f008d12ad4c60a9afb9857642
Buddhi Adhikari <LordDraagonLive@users.noreply.github.com>
2019-11-22 01:50:30 +0000

    Added user model and db context

---
+++ EventPlannerV1/EventContext.cs
@@ -0,0 +1,33 @@
+namespace EventPlannerV1
+{
+    using EventPlannerV1.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class EventContext : DbContext
+    {
+        // Your context has been configured to use a 'EventContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'EventPlannerV1.EventContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'EventContext' 
+        // connection string in the application configuration file.
+        public EventContext()
+            : base("name=EventContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ EventPlannerV1/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerV1.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        [Required]
+        public String Username { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        //[DataType(DataType.Password)]
+        //[Compare("Password")]
+        //[NotMapped]
+        //public string ConfirmPassword { get; set; }
+    }
+}
