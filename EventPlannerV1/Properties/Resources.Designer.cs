c05a386f9f8018d7ca7c4abfedc3b966f71647d6
Buddhi Adhikari <LordDraagonLive@users.noreply.github.com>
2019-11-25 10:34:32 +0000

    Added icons and images to resources

--- EventPlannerV1/Properties/Resources.Designer.cs
+++ EventPlannerV1/Properties/Resources.Designer.cs
@@ -63,9 +63,39 @@ namespace EventPlannerV1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap avatar_default_icon {
+            get {
+                object obj = ResourceManager.GetObject("avatar-default-icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap BackgroundLogin {
             get {
                 object obj = ResourceManager.GetObject("BackgroundLogin", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap contacts_512 {
+            get {
+                object obj = ResourceManager.GetObject("contacts-512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap download {
+            get {
+                object obj = ResourceManager.GetObject("download", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
