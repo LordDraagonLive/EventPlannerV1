a021522951385bc89351f4cd2951140c94c61d16
Buddhi Adhikari <LordDraagonLive@users.noreply.github.com>
2019-11-28 07:53:07 +0000

    Added helper class to store logger method

---
+++ EventPlannerV1/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerV1
+{
+    class Helper
+    {
+        //public void AddToLog(String message)
+        //{
+        //    if (Thread.CurrentThread != m_UIThread)
+        //    {
+        //        // Need for invoke if called from a different thread
+        //        this.Dispatcher.BeginInvoke(
+        //            DispatcherPriority.Normal, (ThreadStart)delegate ()
+        //            {
+        //                AddToLog(message);
+        //            });
+        //    }
+        //    else
+        //    {
+        //        // add this line at the top of the log
+        //        lbLog.Items.Insert(0, message);
+
+        //        // keep only a few lines in the log
+        //        while (lbLog.Items.Count > LOG_MAX_LINES)
+        //        {
+        //            lbLog.Items.RemoveAt(lbLog.Items.Count - 1);
+        //        }
+        //    }
+        //}
+    }
+}
