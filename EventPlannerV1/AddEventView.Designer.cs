fdffb9f666e6d8b2596bfeed02320e3be07744a9
Buddhi Adhikari <LordDraagonLive@users.noreply.github.com>
2019-12-12 08:23:56 +0000

    Colour Scheme Changes

--- EventPlannerV1/AddEventView.Designer.cs
+++ EventPlannerV1/AddEventView.Designer.cs
@@ -49,7 +49,7 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Navy;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -60,7 +60,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Navy;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.label1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Transparent;
             this.label1.Location = new System.Drawing.Point(309, 9);
@@ -78,7 +78,7 @@
             this.addNewEventBtn.FlatAppearance.BorderSize = 2;
             this.addNewEventBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.addNewEventBtn.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addNewEventBtn.ForeColor = System.Drawing.Color.Navy;
+            this.addNewEventBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.addNewEventBtn.Location = new System.Drawing.Point(343, 414);
             this.addNewEventBtn.Name = "addNewEventBtn";
             this.addNewEventBtn.Size = new System.Drawing.Size(155, 39);
@@ -91,7 +91,7 @@
             // 
             this.NoteLbl.AutoSize = true;
             this.NoteLbl.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NoteLbl.ForeColor = System.Drawing.Color.Navy;
+            this.NoteLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.NoteLbl.Location = new System.Drawing.Point(12, 274);
             this.NoteLbl.Name = "NoteLbl";
             this.NoteLbl.Size = new System.Drawing.Size(47, 18);
@@ -100,6 +100,7 @@
             // 
             // eventNoteTxt
             // 
+            this.eventNoteTxt.BackColor = System.Drawing.Color.Azure;
             this.eventNoteTxt.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.eventNoteTxt.ForeColor = System.Drawing.Color.RoyalBlue;
             this.eventNoteTxt.Location = new System.Drawing.Point(137, 274);
@@ -110,6 +111,7 @@
             // 
             // titleTxt
             // 
+            this.titleTxt.BackColor = System.Drawing.Color.Azure;
             this.titleTxt.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titleTxt.ForeColor = System.Drawing.Color.RoyalBlue;
             this.titleTxt.Location = new System.Drawing.Point(137, 132);
@@ -121,7 +123,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Navy;
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.label4.Location = new System.Drawing.Point(12, 229);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(97, 18);
@@ -132,7 +134,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.label3.Location = new System.Drawing.Point(12, 184);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(118, 18);
@@ -143,7 +145,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.label2.Location = new System.Drawing.Point(12, 135);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(97, 18);
@@ -152,9 +154,9 @@
             // 
             // endDtPicker
             // 
-            this.endDtPicker.CalendarForeColor = System.Drawing.Color.Navy;
+            this.endDtPicker.CalendarForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.endDtPicker.CalendarMonthBackground = System.Drawing.Color.Azure;
-            this.endDtPicker.CalendarTitleForeColor = System.Drawing.Color.Navy;
+            this.endDtPicker.CalendarTitleForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.endDtPicker.CustomFormat = "MMMM, dd, yyyy HH:mm";
             this.endDtPicker.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.endDtPicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
@@ -166,9 +168,9 @@
             // startDtPicker
             // 
             this.startDtPicker.CalendarFont = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.startDtPicker.CalendarForeColor = System.Drawing.Color.Navy;
+            this.startDtPicker.CalendarForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.startDtPicker.CalendarMonthBackground = System.Drawing.Color.Azure;
-            this.startDtPicker.CalendarTitleForeColor = System.Drawing.Color.Navy;
+            this.startDtPicker.CalendarTitleForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.startDtPicker.CustomFormat = "MMMM, dd, yyyy HH:mm";
             this.startDtPicker.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startDtPicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
@@ -181,7 +183,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.Navy;
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.label7.Location = new System.Drawing.Point(12, 86);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(81, 18);
@@ -192,7 +194,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.Navy;
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.label8.Location = new System.Drawing.Point(482, 83);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(71, 18);
@@ -202,7 +204,7 @@
             // appointRadBtn
             // 
             this.appointRadBtn.AutoSize = true;
-            this.appointRadBtn.ForeColor = System.Drawing.Color.Navy;
+            this.appointRadBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.appointRadBtn.Location = new System.Drawing.Point(271, 231);
             this.appointRadBtn.Name = "appointRadBtn";
             this.appointRadBtn.Size = new System.Drawing.Size(84, 17);
@@ -215,7 +217,7 @@
             // 
             this.taskRadBtn.AutoSize = true;
             this.taskRadBtn.Checked = true;
-            this.taskRadBtn.ForeColor = System.Drawing.Color.Navy;
+            this.taskRadBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(26)))), ((int)(((byte)(64)))));
             this.taskRadBtn.Location = new System.Drawing.Point(137, 231);
             this.taskRadBtn.Name = "taskRadBtn";
             this.taskRadBtn.Size = new System.Drawing.Size(49, 17);
@@ -245,6 +247,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(239)))), ((int)(((byte)(242)))));
             this.ClientSize = new System.Drawing.Size(800, 514);
             this.Controls.Add(this.repeatEventDropdown);
             this.Controls.Add(this.taskRadBtn);
